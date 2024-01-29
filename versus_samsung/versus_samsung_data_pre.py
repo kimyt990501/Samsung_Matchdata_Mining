fcc9af54ef6ba1969076be980ea9562affd6433f
kimyt990501 <kimyt990501@naver.com>
2024-01-29 15:40:22 +0900

    결측치 삭제

--- versus_samsung/versus_samsung_data_pre.py
+++ versus_samsung/versus_samsung_data_pre.py
@@ -26,6 +26,7 @@ m_3_10 = range(3, 11)
 # 20
 m_5_10 = range(5, 11)
 
+'''
 # 데이터 불러오기
 wr_list = []
 
@@ -44,3 +45,30 @@ wr_2020.columns = ['월', '승률']
 wr_2020 = wr_2020.set_index('월')
 print(wr_2020)
 wr_2020.to_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_winning_rate\\wr_2020.csv', encoding='euc-kr')
+'''
+'''
+# 2001~2023년까지의 경기 데이터를 각 년도별로 합치기
+samsung_base = pd.read_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_match_data\\2020\\samsung_2020_5_match_result.csv', encoding='euc-kr')
+for month in m_5_10:
+    samsung_concat = pd.read_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_match_data\\2020\\samsung_2020_'+ str(month) +'_match_result.csv', encoding='euc-kr')
+    samsung_base = pd.concat([samsung_base, samsung_concat], ignore_index=True)
+samsung_base.to_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_match_data\\all\\samsung_2020_all.csv', encoding='euc-kr')
+#print(samsung_base)
+'''
+'''
+# 각 년도 별 데이터를 하나의 파일로 합치기
+samsung_base = pd.read_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_match_data\\all\\samsung_2001_all.csv', encoding='euc-kr')
+for year in range(2001, 2024):
+    samsung_concat = pd.read_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_match_data\\all\\samsung_'+ str(year) +'_all.csv', encoding='euc-kr')
+    samsung_base = pd.concat([samsung_base, samsung_concat], ignore_index=True)
+
+# 결측치 존재하는 행, 필요없는 칼럼 제거하기
+index = samsung_base[samsung_base['결과'].str.contains('-')].index
+samsung_base = samsung_base.drop(index)
+samsung_base.drop(['Unnamed: 0', '날짜'], axis=1, inplace =True)
+
+# 데이터프레임을 csv 파일로 내보내기
+samsung_base.to_csv('C:\\Users\\user\\Desktop\\samsung_matchdata_mining\\Samsung_Matchdata_Mining\\data\\samsung_match_data\\all\\samsung_all.csv', encoding='euc-kr')
+'''
+
+# 각 구단 별 삼성 상대 승률 구하기
